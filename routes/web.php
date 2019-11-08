c7f2285995c846c87a3e94110076e7981f68273b
Sam Kitano <sam.kitano@gmail.com>
2019-11-08 10:40:25 +0000

    removed deprecated routes

--- routes/web.php
+++ routes/web.php
@@ -28,42 +28,8 @@ Route::group([
 
 });
 
-/*Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('login', [LoginController::class, 'login'])->name('login');
-Route::post('logout', [LoginController::class, 'logout'])->name('logout');
-
-Route::get('request_password', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');*/
 Auth::routes(['verify' => true]);
 
 Route::any('{all}', function () {
     return view('site');
 })->where(['all' => '.*'])->middleware('verified');
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-/* Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/admin', 'HomeController@admin')->name('admin');
-Route::get('/home', 'HomeController@home')->name('home');
-Route::get('/filmes', 'FilmesController@filmes')->name('filmes');
-Route::get('/filme', 'FilmeController@filme')->name('filme');
-Route::get('/perfil', 'PerfilController@perfil')->name('perfil');
-Route::get('/noticias', 'NoticiasController@noticias')->name('noticias');
-Route::get('/noticia', 'NoticiaController@noticia')->name('noticia');
-Route::get('/forum', 'ForumController@forum')->name('forum');
-Route::get('/topics', 'TopicsController@topics')->name('topics');
-Route::get('/topic', 'TopicController@topic')->name('topic');
-Route::get('/premium', 'PremiumController@premium')->name('premium'); */
