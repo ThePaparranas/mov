a7aae4d4503518098fa4320f478f65b2b96f7306
Sam Kitano <sam.kitano@gmail.com>
2019-12-01 09:55:56 +0000

    seo

---
+++ resources/js/components/_head.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  name: 'MetaInfo',
+
+  computed: {
+    currentRoute () {
+      return this.$route.meta.name
+    },
+
+    currentDescription () {
+      return this.$route.meta.description
+    }
+  },
+
+  watch: {
+    currentRoute () {
+      this.setMeta()
+    }
+  },
+
+  beforeMount () {
+    setTimeout(() => {
+      this.setMeta()
+    }, 100)
+  },
+
+  methods: {
+    setMeta () {
+      this.setTitle()
+      this.setDescription()
+    },
+
+    setDescription () {
+      document.getElementsByTagName('meta').description.content = this.currentDescription
+    },
+
+    setTitle () {
+      window.document.title = this.currentRoute
+    }
+  },
+
+  render: () => null
+}
+</script>
